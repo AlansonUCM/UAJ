1000c49e95f18a8fe126923fd6b4cada76d04423
xRyusey <ismafern@ucm.es>
2021-04-26 22:31:44 +0200

    TrackerEvent hecho

--- Sistema telemetria/TrackerLib/src/TrackerEvent.cpp
+++ Sistema telemetria/TrackerLib/src/TrackerEvent.cpp
@@ -1,16 +1,55 @@
-
 #include "TrackerEvent.h"
+#include "json.hpp"
 
-TrackerEvent::TrackerEvent()
+using json = nlohmann::json;
+
+TrackerEvent::TrackerEvent() : type(EventType::Instantaneous), sessionID(0), timestamp(0)
 {
+}
+
+void TrackerEvent::setType(EventType type)
+{
+	this->type = type;
+}
+
+EventType TrackerEvent::getType()
+{
+	return type;
+}
+
+void TrackerEvent::setSessionID(int sessionID)
+{
+	this->sessionID = sessionID;
+}
+
+int TrackerEvent::getSessionID()
+{
+	return sessionID;
+}
+
+void TrackerEvent::setTimestamp(float timestamp)
+{
+	this->timestamp = timestamp;
+}
+
+float TrackerEvent::getTimestamp()
+{
+	return timestamp;
 }
 
 std::string TrackerEvent::toJSON()
 {
-    return std::string();
+	json j;
+	j["type"] = type;
+	j["sessionID"] = sessionID;
+	j["timestamp"] = timestamp;
+	return j.dump();
 }
 
 std::string TrackerEvent::toCSV()
 {
-    return std::string();
+	std::string typeString = "type," + std::to_string((int)type) + "\n";
+	std::string sessionIDString = "sessionID," + std::to_string(sessionID) + "\n";
+	std::string timestampString = "timestamp," + std::to_string(timestamp) + "\n";
+	return typeString + sessionIDString + timestampString;
 }
--- Sistema telemetria/TrackerLib/src/TrackerEvent.h
+++ Sistema telemetria/TrackerLib/src/TrackerEvent.h
@@ -2,15 +2,27 @@
 
 #include <string>
 
+enum class EventType { Instantaneous, TimeBased, Sampling };
+
 class TrackerEvent
 {
 private:
-	//enum EventType
+	EventType type;
+	int sessionID;
+	float timestamp;
 
 public:
 	TrackerEvent();
 
+	void setType(EventType type);
+	EventType getType();
+
+	void setSessionID(int sessionID);
+	int getSessionID();
+
+	void setTimestamp(float timestamp);
+	float getTimestamp();
+
 	std::string toJSON();
 	std::string toCSV();
-
 };
